490517e4f67a1703cdd2596d5774d4b394b469ad
AmitxParmar <amitparmar901@gmail.com>
2023-02-19 18:30:32 +0530

    create README

--- Day 1/hello_world.go
+++ Day 1/hello_world.go
@@ -2,3 +2,9 @@
 package main
 
 // fmt package implements formatted I/O with functions analogous to C's printf and scanf. Pronounce is "fumpt"
+import "fmt"
+
+// Every program should have a min function too, and will be the first executed function
+func main() {
+	fmt.Println("Hello World!!")
+}
