669df1d6c393769e5c868fdb90ae9e67a63a0671
AmitxParmar <amitparmar901@gmail.com>
2023-02-20 19:11:18 +0530

    add tests in closure

---
+++ day04/closures_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+// TestSequence tests if sequence is generated correctly
+
+func TestSequence(t *testing.T) {
+	// initialize the sequence (closure is returned)
+	nextInt := intSequence()
+	for i := 1; i < 10; i++ {
+		//call nextInt n times
+		if n := nextInt(); n != i {
+			t.Errorf("expected %d but %d was obtained", i, n)
+		}
+	}
+}
