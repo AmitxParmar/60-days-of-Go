6d662e1a627dfe044ea67c62e6daff519d7b139b
AmitxParmar <amitparmar901@gmail.com>
2023-03-02 20:56:05 +0530

    #day11 - complete

---
+++ day11/card/models.go
@@ -0,0 +1,7 @@
+package cards
+
+// Card is a item in todo list
+type Card struct {
+	Title string `json: "title"`
+	Text  string `json: "text"`
+}
---
+++ day11/card/serializers.go
@@ -0,0 +1,36 @@
+package cards
+
+import (
+	"fmt"
+	"strings"
+)
+
+// CardSerializer is card serializer
+type CardSerializer struct {
+	Card
+	errors []string // for while an array of strings `json:"errors"`
+}
+
+// validate verify if content of a card is valid
+func (p *CardSerializer) Validate() bool {
+	if p.Title == "" {
+		p.errors = append(p.errors, "title is empty")
+	}
+	if p.Text == "" {
+		p.errors = append(p.errors, "text is empty")
+	}
+	return p.Title != "" && p.Text != ""
+}
+
+// Errors are list of errors
+// as string for while
+func (p CardSerializer) Errors() string {
+	return strings.Join(p.errors, " and ")
+}
+
+// Save persists a card
+
+// Not implement yet
+func (p CardSerializer) Save() {
+	fmt.Println("Save")
+}
