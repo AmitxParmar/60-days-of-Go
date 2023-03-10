85a6e0dd934a242be4a1ef5ddbbcc9dc4ac03063
AmitxParmar <amitparmar901@gmail.com>
2023-03-10 22:48:28 +0530

    complete: #Day15

---
+++ day15/database/database.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"github.com/jmoiron/sqlx"
+	//register sqlite 3 driver
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sqlx.DB
+
+// MustConnect initialize the database in memory
+func MustConnect() {
+	db = sqlx.MustConnect("sqlite3", ":memory:")
+}
+
+// Get returns the database connection
+func Get() *sqlx.DB {
+	return db
+}
